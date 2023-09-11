b91a98c96dc1399617b9b602b7f3d2d5af69ed1e
ClausWong <wangjiaw@chanjet.com>
2023-09-11 19:55:46 +0800

    代理模式

--- proxy.js
+++ proxy.js
@@ -5,7 +5,17 @@ const Girl = function(name) {
 const Boy = function (girl) {
     this.girl = girl
     this.sendGift = function(gift) {
-        console.log(`${this.girl.name}，给你送的礼物${gift}`)
+        console.log(`hi，${this.girl.name}，给你送的礼物：${gift}`)
     }
 }
 
+const ProxyObj = function(girl) {
+    this.girl = girl
+    this.sendGift = function(gift) {
+        (new Boy(this.girl)).sendGift(gift)
+    }
+}
+
+const girl = new Girl('小鸟依人')
+const proxy = new ProxyObj(girl)
+proxy.sendGift('尤加利')
