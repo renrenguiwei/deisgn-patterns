4ab885d416dafbe1c181a15e266c522001c6eb62
ClausWong <726787295@qq.com>
2023-09-13 16:41:15 +0800

    设计模式 装饰器 @装饰参数

--- src/decorator/decorator@.js
+++ src/decorator/decorator@.js
@@ -5,8 +5,21 @@ class Boy {
     }
 }
 
+/**
+ * @param target Boy对象
+ * @param key    被装饰的方法名
+ * @param descriptor    描述装饰的方法
+ */
 function run (target, key, descriptor) {
     console.log(target, key, descriptor)
+    // {}
+    // speak
+    // {
+    //   value: [Function: speak],
+    //   writable: true,
+    //   enumerable: false,
+    //   configurable: true
+    // }
     console.log('跑步')
 }
 
