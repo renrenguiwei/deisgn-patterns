541b5709dce96483ca8de79f5a18d0486bc36e67
ClausWong <726787295@qq.com>
2023-09-13 16:36:32 +0800

    设计模式 装饰器 利用@

---
+++ build/decorator@.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _desc, _value, _class;
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _applyDecoratedDescriptor(target, property, decorators, descriptor, context) {
+    var desc = {};
+    Object['ke' + 'ys'](descriptor).forEach(function (key) {
+        desc[key] = descriptor[key];
+    });
+    desc.enumerable = !!desc.enumerable;
+    desc.configurable = !!desc.configurable;
+
+    if ('value' in desc || desc.initializer) {
+        desc.writable = true;
+    }
+
+    desc = decorators.slice().reverse().reduce(function (desc, decorator) {
+        return decorator(target, property, desc) || desc;
+    }, desc);
+
+    if (context && desc.initializer !== void 0) {
+        desc.value = desc.initializer ? desc.initializer.call(context) : void 0;
+        desc.initializer = undefined;
+    }
+
+    if (desc.initializer === void 0) {
+        Object['define' + 'Property'](target, property, desc);
+        desc = null;
+    }
+
+    return desc;
+}
+
+var Boy = (_class = function () {
+    function Boy() {
+        _classCallCheck(this, Boy);
+    }
+
+    _createClass(Boy, [{
+        key: 'speak',
+        value: function speak() {
+            console.log('唱歌');
+        }
+    }]);
+
+    return Boy;
+}(), (_applyDecoratedDescriptor(_class.prototype, 'speak', [run], Object.getOwnPropertyDescriptor(_class.prototype, 'speak'), _class.prototype)), _class);
+
+
+function run() {
+    console.log('跑步');
+}
+
+var boy = new Boy();
+boy.speak();
---
+++ src/decorator/decorator@.js
@@ -0,0 +1,13 @@
+class Boy {
+    @run
+    speak () {
+        console.log('唱歌')
+    }
+}
+
+function run () {
+    console.log('跑步')
+}
+
+const boy = new Boy()
+boy.speak()
