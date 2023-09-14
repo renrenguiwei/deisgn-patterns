99a9d15026fae629033d59700dd484bca90038be
ClausWong <726787295@qq.com>
2023-09-14 21:58:35 +0800

    设计模式 责任链

---
+++ src/chain_of_responsibility.js
@@ -0,0 +1,57 @@
+// 责任链模式
+function order500(orderType, isPay, count) {
+    if (orderType === 1 && isPay) {
+        console.log('获得100元')
+    } else {
+        return 'goNext'
+    }
+}
+function order400(orderType, isPay, count) {
+    if (orderType === 2 && isPay) {
+        console.log('获得40元')
+    } else {
+        return 'goNext'
+    }
+}
+function order200(orderType, isPay, count) {
+    if (orderType === 3 && isPay) {
+        console.log('获得20元')
+    } else {
+        return 'goNext'
+    }
+}
+function orderNormal(orderType, isPay, count) {
+    if (count > 0) {
+        console.log('奖励10元')
+    } else {
+        console.log('谢谢惠顾')
+    }
+}
+
+function Chain(fn) {
+    this.fn = fn
+    this.nextStep = null
+}
+Chain.prototype.setNextStep = function (step) {
+    this.nextStep = step
+}
+
+Chain.prototype.passRequest = function () {
+    const ret = this.fn.apply(this, arguments)
+    if (ret === 'goNext') {
+        this.nextStep &&
+        this.nextStep.passRequest.apply(this.nextStep, arguments)
+    }
+}
+
+const chainOrder500 = new Chain(order500)
+const chainOrder400 = new Chain(order400)
+const chainOrder200 = new Chain(order200)
+const chainOrderNormal = new Chain(orderNormal)
+
+chainOrder500.setNextStep(chainOrder400)
+chainOrder400.setNextStep(chainOrder200)
+chainOrder200.setNextStep(chainOrderNormal)
+
+chainOrder500.passRequest(2, true, 0)
+
