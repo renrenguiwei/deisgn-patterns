b83bd5dc53459bff9e0c43810ed89b6c7d7236cd
ClausWong <726787295@qq.com>
2023-09-15 16:12:22 +0800

    设计模式 单例-饿汉模式-防止类被实例化

--- src/singleton_hungry.js
+++ src/singleton_hungry.js
@@ -1,6 +1,7 @@
 // 饿汉模式：调用类时实例化
 class HungrySingletonTest {
     main() { // javascript标准语法 默认是public，是不需要额外加
+        new HungrySingleton()
         const HungrySingleton1 = HungrySingleton.getInstance();
         const HungrySingleton2 = HungrySingleton.getInstance();
         console.log(HungrySingleton1 === HungrySingleton2);
@@ -9,6 +10,13 @@ class HungrySingletonTest {
 
 class HungrySingleton {
     static _instance  = new HungrySingleton()
+
+    constructor() {
+        // 防止类被再次实例化
+        if (HungrySingleton._instance) {
+            throw new Error("Singleton instance already exists. Use getInstance() method.");
+        }
+    }
 
     static getInstance() {
         return this._instance
